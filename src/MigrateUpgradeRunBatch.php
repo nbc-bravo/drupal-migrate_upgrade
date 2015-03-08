92bc3fd962e92dfd12c266b12973e7bae6ed31eb
Ben Dougherty <ben@dougherty.net.au>
2015-03-09 09:30:15 +1100

    Track the counts during the run, make the output simpler.

--- src/MigrateUpgradeRunBatch.php
+++ src/MigrateUpgradeRunBatch.php
@@ -23,44 +23,48 @@ class MigrateUpgradeRunBatch {
     if (!isset($context['sandbox']['migration_ids'])) {
       $context['sandbox']['max'] = count($initial_ids);
       $context['sandbox']['migration_ids'] = $initial_ids;
+      $context['results']['failures'] = 0;
+      $context['results']['successes'] = 0;
     }
     $migration_id = reset($context['sandbox']['migration_ids']);
     $migration = entity_load('migration', $migration_id);
     if ($migration) {
       $messages = new MigrateMessageCapture();
       $executable = new MigrateExecutable($migration, $messages);
+
+      // @TODO, if label isn't required we could implement this logic on the
+      // migration entity itself.
       $migration_name = $migration->label() ? $migration->label() : $migration_id;
-      \Drupal::logger('migrate_upgrade')->notice('Importing @migration',
-                         array('@migration' => $migration_name));
+
+      static::logger()->notice('Importing @migration', array('@migration' => $migration_name));
       $migration_status = $executable->import();
+
       switch ($migration_status) {
         case MigrationInterface::RESULT_COMPLETED:
-          $context['message'] = t('Imported @migration',
-            array('@migration' => $migration_name));
-          $context['results'][$migration_name] = 'success';
-          \Drupal::logger('migrate_upgrade')->notice('Imported @migration',
-                   array('@migration' => $migration_name));
+          $context['message'] = t('Imported @migration', array('@migration' => $migration_name));
+          $context['results']['failures']++;
+          static::logger()->notice('Imported @migration', array('@migration' => $migration_name));
           break;
+
         case MigrationInterface::RESULT_INCOMPLETE:
-          $context['message'] = t('Importing @migration',
-            array('@migration' => $migration_name));
+          $context['message'] = t('Importing @migration', array('@migration' => $migration_name));
           break;
+
         case MigrationInterface::RESULT_STOPPED:
           $context['message'] = t('Import stopped by request');
           break;
+
         case MigrationInterface::RESULT_FAILED:
-          $context['message'] = t('Import of @migration failed',
-            array('@migration' => $migration_name));
-          $context['results'][$migration_name] = 'failure';
-          \Drupal::logger('migrate_upgrade')->error('Import of @migration failed',
-                   array('@migration' => $migration_name));
+          $context['message'] = t('Import of @migration failed', array('@migration' => $migration_name));
+          $context['results']['successes']++;
+          static::logger()->error('Import of @migration failed', array('@migration' => $migration_name));
           break;
+
         case MigrationInterface::RESULT_SKIPPED:
-          $context['message'] = t('Import of @migration skipped due to unfulfilled dependencies',
-            array('@migration' => $migration_name));
-          \Drupal::logger('migrate_upgrade')->error('Import of @migration skipped due to unfulfilled dependencies',
-                   array('@migration' => $migration_name));
+          $context['message'] = t('Import of @migration skipped due to unfulfilled dependencies', array('@migration' => $migration_name));
+          static::logger()->error('Import of @migration skipped due to unfulfilled dependencies', array('@migration' => $migration_name));
           break;
+
         case MigrationInterface::RESULT_DISABLED:
           // Skip silently if disabled.
           break;
@@ -71,63 +75,72 @@ class MigrateUpgradeRunBatch {
         $context['message'] .= "<br />\n" . $message;
       }
 
-      // Unless we're continuing on with this migration, take it off the list.
+      // @TODO, why would we call ->import again, can we not just use the
+      // $migration_status variable?
       if ($executable->import() != MigrationInterface::RESULT_INCOMPLETE) {
+        // Unless we're continuing on with this migration, take it off the list.
         array_shift($context['sandbox']['migration_ids']);
       }
     }
     else {
       array_shift($context['sandbox']['migration_ids']);
     }
+
+    // @TODO, does this work when you have disabled migrations? max never gets
+    // updated after the first run.
     $context['finished'] = 1 - count($context['sandbox']['migration_ids']) / $context['sandbox']['max'];
   }
 
   /**
-   * @param $success
-   * @param $results
-   * @param $operations
-   * @param $elapsed
+   * A helper method to grab the logger using the migrate_upgrade channel.
+   *
+   * @return \Psr\Log\LoggerInterface
+   *   The logger instance.
    */
-  public static function finished($success, $results, $operations, $elapsed) {
-    self::displayResults($results);
+  protected static function logger() {
+    return \Drupal::logger('migrate_upgrade');
   }
 
   /**
-   * Display counts of success/failures.
+   * Implementation of the Batch API finished method.
+   */
+  public static function finished($success, $results, $operations, $elapsed) {
+    static::displayResults($results);
+  }
+
+  /**
+   * Display counts of success/failures on the migration upgrade complete page.
    *
    * @param $results
+   *   An array of result data built during the batch.
    */
   protected static function displayResults($results) {
-    $successes = $failures = 0;
-    $status_type = 'status';
-    foreach ($results as $result) {
-      if ($result == 'success') {
-        $successes++;
-      }
-      else {
-        $failures++;
-      }
+    $successes = $results['successes'];
+    $failures = $results['failures'];
+    $translation = \Drupal::translation();
+
+    // If we had any successes lot that for the user.
+    if ($successes > 0) {
+      drupal_set_message(t('Import completed @count successfully.', array('@count' => $translation->formatPlural($successes, '1 migration', '@count migrations'))));
     }
-    if ($successes > 0 && $failures == 0) {
-      drupal_set_message(t('Import complete.'));
-      drupal_set_message(t('@count succeeded',
-        array('@count' => \Drupal::translation()->formatPlural($successes,
-          '1 migration', '@count migrations'))));
-      drupal_set_message(t('Congratulations, you upgraded Drupal!'));
-    }
+
+    // If we had failures, log them and show the migration failed.
     if ($failures > 0) {
+      drupal_set_message(t('@count failed', array('@count' => $translation->formatPlural($failures, '1 migration', '@count migrations'))), 'error');
       drupal_set_message(t('Import process not completed'), 'error');
-      drupal_set_message(t('@count succeeded',
-        array('@count' => \Drupal::translation()->formatPlural($successes,
-          '1 migration', '@count migrations'))), 'error');
-      drupal_set_message(t('@count failed',
-        array('@count' => \Drupal::translation()->formatPlural($failures,
-          '1 migration', '@count migrations'))), 'error');
       $status_type = 'error';
     }
+    else {
+      // Everything went off without a hitch. We may not have had successes but
+      // we didn't have failures so this is fine.
+      drupal_set_message(t('Congratulations, you upgraded Drupal!'));
+      $status_type = 'status';
+    }
+
     if (\Drupal::moduleHandler()->moduleExists('dblog')) {
       $url = new URL('migrate_upgrade.log');
       drupal_set_message(\Drupal::l(t('Review the detailed migration log'), $url), $status_type);
     }
   }
+
 }
